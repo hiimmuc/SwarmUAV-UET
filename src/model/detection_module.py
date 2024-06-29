c9ff6f001fe12f359b8b29b3c98271dca8b6a50b
hiimmuc <nam1811.hust@gmail.com>
2024-06-29 13:18:16 +0900

    add logs

--- src/model/detection_module.py
+++ src/model/detection_module.py
@@ -1,18 +1,25 @@
 import time
 import matplotlib.pyplot as plt
+import argparse
+from typing import Dict, Any, Optional, Union
 
 from model.model import *
 
 
 class ObjectDetection:
-    def __init__(self):
+    def __init__(self, config: Dict[str, Any]):
         super(ObjectDetection, self).__init__()
         t0 = time.time()
 
-        self.detection_engine = DetectionModel()
+        self.model_config = config
+        self.detection_engine = DetectionModel(model_cfg=self.model_config['model_cfg'],
+                                               model_weight=self.model_config['model_weight'],
+                                               label=self.model_config['label'])
 
         print("Load model time:", time.time() - t0)
-        t1 = time.time()
+
+    def image_detect(self, image_path=None, image=None, save_results=False):
+        pass
 
     def video_detect(self, source=0):
         cap = cv2.VideoCapture(source)
@@ -37,3 +44,20 @@ class ObjectDetection:
                 break
         cap.release()
         cv2.destroyAllWindows()
+
+
+parser = argparse.ArgumentParser(description="Detection model")
+if __name__ == "__main__":
+    parser.add_argument('--config_path', type=str,
+                        default='./checkpoints/MobileNetSSD_deploy.prototxt')
+    parser.add_argument('--weight_path', type=str,
+                        default='./checkpoints/MobileNetSSD_deploy.caffemodel')
+    parser.add_argument('--label_path', type=str,
+                        default='./checkpoints/labels.name')
+    args = parser.parse_args()
+
+    model_config = {'model_cfg': args.config_path,
+                    'model_weight': args.weight_path,
+                    'label_path': args.label_path}
+
+    model = ObjectDetection(model_config)
